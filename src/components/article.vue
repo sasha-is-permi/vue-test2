defb196893cfe8d452621fee6094ae6ffa546fd8
Alexandr <sasha-is-permi@mail.ru>
2021-09-16 16:09:48 +0500

    2

--- src/components/article.vue
+++ src/components/article.vue
@@ -1,6 +1,10 @@
 <template>
-<div>
-   <h1 style="text-align:center"> Статья </h1>
+<div class="border">
+   <h2 class="text-center"> {{theme}} &nbsp;&nbsp;&nbsp;  {{date}}</h2>
+    <div class="text" v-html="content"></div>
+    <div class="link">
+     <router-link class="red " to="/Main">  Вернуться к списку статей   </router-link> 
+    </div> 
  </div>   
 </template>
 
@@ -8,8 +12,10 @@
 export default{
  data(){   
  return{
- 
- 
+ date:     this.$route.params.article.date || " " ,
+ theme: this.$route.params.article.theme || " ",
+ content:  this.$route.params.article.content || " " 
+
  }
  }
 }
@@ -17,5 +23,33 @@ export default{
 
 <style scoped>
 
+.red{
+    color:#c00;
+}
+
+
+.text-center{
+  text-align: center;
+}
+
+.text{
+  width:60%;
+  margin: 0 auto;
+  font-size:22px;
+}
+
+.link{
+  margin: 0 auto;
+  width:250px;
+  font-size:22px;
+}
+
+.border{
+  width:80%;
+  height:80%;
+  margin: 0 auto;
+
+
+}
 
 </style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,21 +1,74 @@
 <template>
-<div>
-   <h1 style="text-align:center"> Главная страница </h1>
- </div>   
+    <div class="articles">
+       <div class="search">
+        <span> Поиск по словам, встречающихся в статье: </span>
+        <input type="text" v-model="articleSearch"/>
+        </div>
+        <h2 class="header">Статьи:</h2>
+        <div class="text">
+        <p class="article"  v-for="article in articles" :key="article.date">
+              <router-link 
+        :to="{name:'Article' ,params:{article:article}}"    
+        >   {{article.date}} {{article.author.name}} &nbsp;&nbsp;&nbsp; {{article.theme}} &nbsp;&nbsp;&nbsp; {{article.content.substring(0,50)+"..."}} </router-link>
+        </p>  
+       <div class="link">
+     <router-link class="red " to="/">  Вернуться к авторизации  </router-link> 
+    </div> 
+        </div>
+    </div>
 </template>
 
+
 <script>
-export default{
- data(){   
- return{
+   export default{
+       data() {
+         return{
+           articleSearch:""    
+         }
+       },
+           
+              computed: {           
+              articles() {
+                  let articles2 = this.$store.getters.articles;
+                  console.log("articles", articles2)
+                
+                  articles2 = articles2.filter(item => {
+                  return item.content.toUpperCase().indexOf(this.articleSearch.toUpperCase())!==-1;
+                    })
+
+                  console.log("articles2", articles2)
+                    return articles2;
+            }
+            }
+                
+                }
+
+                
+            
+
  
- 
- }
- }
-}
+
 </script>
 
 <style scoped>
+  .search{
+  width:350px;
+  margin: 0 auto;
+  font-size:22px;
+  }
+  .text{
+  width:60%;
+  margin: 0 auto;
+  font-size:22px;
+  }
+  .header{
+    text-align: center;
+  }
 
+  .link{
+  margin: 0 auto;
+  width:250px;
+  font-size:22px;
+}
 
 </style>
